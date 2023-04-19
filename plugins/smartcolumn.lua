0407c7721caa7f612a0a2dabc217959b6fc83d7d
zensimilia <zensimilia@gmail.com>
2023-04-19 16:02:44 +0300

    Install smartcolumn

---
+++ plugins/smartcolumn.lua
@@ -0,0 +1,10 @@
+return {
+    "m4xshen/smartcolumn.nvim",
+    enabled = true,
+    lazy = false,
+    opts = {
+        colorcolumn = "80",
+        scope = "file",
+        disabled_filetypes = { "help", "text", "markdown", "NvimTree" },
+    },
+}
